af83c47f68cc6302d296644952110f4d35ce929f
zakan9 <savukynas.nojus@gmail.com>
2026-02-03 12:00:12 +0200

    Added environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayersModule } from './players/players.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { Player } from './interfaces/player.interface';
-
-const API_KEY = '';
-const BASE_URL = 'https://api.balldontlie.io/v1';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PlayersService {
+  constructor(private configService: ConfigService) {}
+
+  getApiKey() {
+    return this.configService.get<string>('API_KEY');
+  }
+
+  getBaseUrl() {
+    return this.configService.get<string>('BASE_URL');
+  }
+
   async getPlayers(): Promise<Player[]> {
     const response = await axios.get<{ data: Player[] }>(
-      `${BASE_URL}/players`,
+      `${this.getBaseUrl()}/players`,
       {
         headers: {
-          Authorization: API_KEY,
+          Authorization: this.getApiKey(),
         },
       },
     );
