6e61377f58ba02b6b389e21514604ae462ed9ed4
zakan9 <savukynas.nojus@gmail.com>
2026-02-12 09:47:12 +0200

    Added tests for create and delete

--- test/players.e2e-spec.ts
+++ test/players.e2e-spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from '../src/app.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import request from 'supertest';
+import { IPlayer } from 'src/players/interfaces/player.interface';
 
 describe('Players Endpoints (e2e)', () => {
   let app: INestApplication;
@@ -52,6 +53,52 @@ describe('Players Endpoints (e2e)', () => {
     return request(app.getHttpServer())
       .post('/players')
       .send(playerInfo)
+      .expect(201)
+      .expect((res) => {
+        const body = res.body as IPlayer;
+        expect(body.first_name).toEqual('Nojus');
+        expect(body.last_name).toEqual('Savukynas');
+        expect(body.position).toEqual(['G', 'F']);
+        expect(body.height).toEqual(177);
+        expect(body.weight).toEqual(65);
+        expect(body.jersey_number).toEqual('21');
+        expect(body.college).toEqual('Alytus');
+        expect(body.country).toEqual('LT');
+        expect(body.draft_year).toEqual(2024);
+        expect(body.draft_round).toEqual(1);
+        expect(body.draft_number).toEqual(1);
+      });
+  });
+
+  it('DELETE /players/:id', async () => {
+    const playerInfo = {
+      first_name: 'Kasparas',
+      last_name: 'Jonaitis',
+      position: ['F', 'C'],
+      height: 210,
+      weight: 130,
+      jersey_number: '11',
+      college: 'Kaunas Zalgiris',
+      country: 'LT',
+      draft_year: 2022,
+      draft_round: 2,
+      draft_number: 30,
+    };
+
+    const createResponse = await request(app.getHttpServer())
+      .post('/players')
+      .send(playerInfo)
       .expect(201);
+
+    const createdPlayer = createResponse.body as IPlayer & { _id: string };
+    expect(createdPlayer._id).toBeDefined();
+
+    await request(app.getHttpServer())
+      .delete(`/players/${createdPlayer._id}`)
+      .expect(200);
+
+    await request(app.getHttpServer())
+      .get(`/players/${createdPlayer._id}`)
+      .expect(404);
   });
 });
