74105bee01e58648898d9642c99ad4cc07e5e459
zakan9 <savukynas.nojus@gmail.com>
2026-02-18 18:37:45 +0200

    Made functions better

--- src/players/utility/convert-lbs-to-kg.utility.ts
+++ src/players/utility/convert-lbs-to-kg.utility.ts
@@ -1,8 +1,12 @@
-export function convertLbsToKg(weight: string): number | null {
-  const parsed = parseInt(weight, 10);
-  if (!Number.isNaN(parsed)) {
-    return Math.round(parsed * 0.45);
-  } else {
+export function convertLbsToKg(weight: string | number): number | null {
+  if (typeof weight === 'string') {
+    const parsed = parseInt(weight, 10);
+    if (!Number.isNaN(parsed)) {
+      return Math.round(parsed * 0.45);
+    }
     return null;
+  } else if (typeof weight === 'number') {
+    return Math.round(weight * 0.45);
   }
+  return null;
 }
